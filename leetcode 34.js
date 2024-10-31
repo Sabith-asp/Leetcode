4d059f5846ade2a022cfb0f346c06dfd9c4f41ce
Sabith-asp <abdullasabithpulli@gmail.com>
2024-10-31 10:05:46 +0530

    Problem : Find First and Last Position of Element in Sorted Array

---
+++ leetcode 34.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    let first=nums.indexOf(target)
+    let last=nums.lastIndexOf(target)
+    return [first,last]
+};
