78b97d9c4674f8afab494040e0a9072aa11e96ba
Abdulla Sabith <96559707+Sabith-asp@users.noreply.github.com>
2024-11-20 09:53:40 +0530

    Time: 59 ms (29.32%), Space: 49.6 MB (25.01%) - LeetHub

--- 2761-array-reduce-transformation/2761-array-reduce-transformation.js
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -5,21 +5,10 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    let result=0
-            if(nums.length===0){
-                result+=init
-            }
-        
-           else{
-            for(i=0;i<nums.length;i++){
-                i===0?  result+= fn(init,nums[i]): result=fn(result,nums[i])
-            }
-                
-            }
-              
-    
-    
-    
-    return result
-    
+    let acc=init
+    for(let i=0;i<nums.length;i++){
+        let result=fn(acc,nums[i])
+        acc=result
+    }
+    return acc
 };
