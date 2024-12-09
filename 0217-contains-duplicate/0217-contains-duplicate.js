26bc1903a48f0a187a8736bbe53f67c6663f4a78
Abdulla Sabith <96559707+Sabith-asp@users.noreply.github.com>
2024-12-09 09:49:42 +0530

    Time: 15 ms (47.28%), Space: 61.2 MB (54.24%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,12 +3,5 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let counts={}
-    for(const num of nums){
-        counts[num]=(counts[num]||0)+1
-    }
-    if(Object.values(counts).find((item)=>item>=2)){
-        return true
-    }
-    return false
+return [...new Set(nums)].length!==nums.length
 };
