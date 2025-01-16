9ba9b8204edb2942d180edfff568cb861aea6e10
Abdulla Sabith <96559707+Sabith-asp@users.noreply.github.com>
2025-01-16 19:45:51 +0530

    Time: 2 ms (58.89%), Space: 54.2 MB (57.93%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let nonZeroIndex = 0;
+    for(i=0;i<nums.length;i++){
+        if(nums[i]!==0){
+            [nums[nonZeroIndex], nums[i]] = [nums[i], nums[nonZeroIndex]];
+            nonZeroIndex++;
+
+        }
+    }
+};
