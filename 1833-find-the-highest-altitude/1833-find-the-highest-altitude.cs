385d49cea5c7faaff72488fac0b5e0c497dc3e41
Abdulla Sabith <96559707+Sabith-asp@users.noreply.github.com>
2025-03-13 21:14:07 +0530

    Time: 2 ms (12.52%), Space: 41.1 MB (88.7%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cs
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cs
@@ -5,7 +5,7 @@ public class Solution {
         alts.Add(0);
         for(int i=0;i<gain.Length;i++){
             alts.Add(prev+gain[i]);
-            prev=prev+gain[i];
+            prev=alts[i+1];
         }
         return alts.Max(x=>x);
     }
