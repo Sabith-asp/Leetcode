bdb6d450088875aab1b627da229d0ec1241bbf82
Abdulla Sabith <96559707+Sabith-asp@users.noreply.github.com>
2025-04-10 20:57:24 +0530

    Time: 0 ms (100%), Space: 29.3 MB (9.88%) - LeetHub

--- 0476-number-complement/0476-number-complement.cs
+++ 0476-number-complement/0476-number-complement.cs
@@ -1,16 +1,16 @@
 public class Solution {
     public int FindComplement(int num) {
-        Console.WriteLine(Convert.ToString(num,2));
-        char[] split=Convert.ToString(num,2).ToCharArray();
-        for(int i=0;i<split.Length;i++){
-            if(split[i]=='0'){
-                split[i]='1';
-            }else{
-                split[i]='0';
-            }
-        }
-    // Console.WriteLine(int.Parse(string.Join("",split)));
+    //    var arr= num.ToString().ToCharArray();
+       var arr = Convert.ToString(num, 2).ToCharArray();
+       for(int i=0;i<arr.Length;i++){
 
-       return Convert.ToInt32(string.Join("", split), 2);
+        if(arr[i]=='1'){
+            arr[i]='0';
+        }else{
+            arr[i]='1';
+        }
+       }
+       string flippedBinary = new string(arr);
+        return Convert.ToInt32(flippedBinary, 2);
     }
 }
